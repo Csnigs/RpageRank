2680ff5faf6552ecb54fcf1ddbfaa7b8dce9e617
Snigs <charles.sonigo+git@gmail.com>
2015-02-22 20:15:47 +0800

    fonctionne sur test set

--- pageRank2.R
+++ pageRank2.R
@@ -21,7 +21,7 @@ reID<-function(dataT){
       IDmap
 }
 
-pageRank<-function(dataT){
+pageRank<-function(dataT,epsil=0.0001){
       #map les ID
       IDmap <- reID(dataT)
       
@@ -38,36 +38,47 @@ pageRank<-function(dataT){
       dataT[ , `:=`(outD = .N) , by = from]
       
       #Init rank1 et rank2
+      rank<-matrix(c(rep(1/nNodes,nNodes),rep(0,nNodes)),ncol = 2)
       rank1<-rep(1/nNodes,nNodes)
       rank2<-rep(0,nNodes)
-      
-      #Iteration calcul de r'
-      for (j in 1:nNodes){
-            oldj<-getoldID(j)
-            rankj<-0
-            toJ<-dataT[to==oldj,]
-            if(nrow(toJ)!=0){
-                  for (k in seq(nrow(toJ))){
-                        ri<-rank1[getnewID(toJ[k,from])]
-                        di<-toJ[k,outD]
-                        rankj<- rankj + 0.8*ri/di
+      oddIt<-2
+      while (distV(rank[,1],rank[,2])>epsil){
+            #Iteration calcul de r'
+            for (j in 1:nNodes){
+                  oldj<-getoldID(j)
+                  rankj<-0
+                  toJ<-dataT[to==oldj,]
+                  if(nrow(toJ)!=0){
+                        for (k in seq(nrow(toJ))){
+                              ri<-rank[getnewID(toJ[k,from]),3-oddIt]
+                              di<-toJ[k,outD]
+                              rankj<- rankj + 0.8*ri/di
+                        }
                   }
+                  else rankj<-0
+                  rank[j,oddIt]<-rankj
             }
-            else rankj<-0
-            rank2[j]<-rankj
+            
+            #soustraction de S a r' 
+            S<-0
+            for (i in seq(nNodes)){
+                  S<-S+ rank[i,oddIt]
+            }
+            for (i in seq(nNodes)){
+                  rank[i,oddIt]<-rank[i,oddIt] +(1-S)/nNodes
+            }
+            oddIt<-3-oddIt
       }
+      cbind(rank[,3-oddIt],IDmap[,oldID])
+
       
-      #soustraction de S a r' 
-      S<-0
-      for (i in seq(nNodes)){
-            S<-S+ rank2[i]
+}
+
+distV<-function(vect1, vect2){
+      res<-0
+      for (i in 1:length(vect1)){
+            res<- res+ abs(vect1[i]-vect2[i])
       }
-      for (i in seq(nNodes)){
-            rank2[i]<-rank2[i] +(1-S)/nNodes
-      }
-      
-      rank2
-      #data[order(to)]
-      #dataT[to==1,]
+      res
       
 }
