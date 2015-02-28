ecf22ae84b903b46b8404d47ff46730c590d608b
Snigs <charles.sonigo+git@gmail.com>
2015-02-28 12:38:46 +0800

    version fonctionnelle mais pb de memoire

--- pageRank2.R
+++ pageRank2.R
@@ -2,7 +2,7 @@ initPageRank<-function(){
       setwd("~/Cours/Coursera/Mining Massive Datasets/googleGraph")
       library(data.table)
       library(dplyr)    
-      data<<-data.table(read.csv("web-Google.txt",header = F,skip=4,sep="\t",col.names=c("from","to")))
+      dataP<<-data.table(read.csv("web-Google.txt",header = F,skip=4,sep="\t",col.names=c("from","to")))
 }
 
 readData<-function(file){
@@ -28,7 +28,9 @@ reID<-function(dataT){
       IDmap
 }
 
-pageRank<-function(dataT,epsil=0.0001){
+pageRank<-function(dataP,epsil=0.1){
+      dataT<-copy(dataP)
+      iteration<-0
       #map les ID
       IDmap <- reID(dataT)
       setkey(IDmap,oldID)
@@ -44,75 +46,56 @@ pageRank<-function(dataT,epsil=0.0001){
       #ajoute le outDegree au data
       nNodes<-nNode(dataT)
       dataT[ , `:=`(outD = .N) , by = from]
-      dataT<-mutate(dataT,preri = 0,R1 = 1/nNodes,R2 =0)
-      
+      #dataT<-mutate(dataT,preri = 0,R1 = 1/nNodes,R2 =0,newIDfrom = getnewID(from),newIDto = getnewID(to))
       #Init rank1 et rank2
-      R1<-rep(1/nNodes,nNodes)
-      R2<-rep(0,nNodes)
-      rank<-data.table(R1,R2)
-
-      turn<-T
-      setkey(dataT,to)
+      rank<-copy(IDmap)
+      prevrank<-copy(IDmap)
+      #rank<-mutate(rank,Rank= 1/nNodes)
+      rank[,Rank:=1/nNodes]
+      prevrank[,Rank:=0]
+      dist<-distV(rank[,Rank],prevrank[,Rank])
+      print(dist)
      # setkey(dataT,)
-      while (distV(dataT[,R1],dataT[,R2])>epsil){
-            if (turn){
-                  outDi<-dataT[1,preri]
-                  dataT[ , `:=`(preri = R1/outD)]
-                  outDi<-dataT[1,preri]
-                  turn<-!turn
-            }
-            ## AGREGATIOOONNNNNNN
+      while (dist>epsil){
+
+            setkey(dataT,from)
+            dataT<-rank[dataT,nomatch=NA]
+            setnames(dataT,"oldID","from")
+            dataT[,newID:=NULL]
+            dataT[ , `:=`(preri = Rank*0.8/outD)]
+            agregPreri<-data.table(group_by(dataT,to) %>%summarise( agRi = sum(preri)))
+
+            prevrank<-rank          
             
-#             #Iteration 2
-#             
-#             ranki<-rank[[getnewID(dataT[1,from]),3-oddIt]]
-#             outDi<-dataT[1,preri]
-#             oldIDi<- dataT[1,from]
-#             for (rowNum in seq(nrow(dataT))) {  
-#                   
-#                   if(dataT[rowNum,from] == oldIDi){
-#                          set(dataT,rowNum,4L,outDi*ranki)
-#                        # set(dataT,rowNum,4L,dataT[rowNum,preri]*rank[[getnewID(dataT[rowNum,from]),3-oddIt]])
-#                        # dataT[rowNum,preri:= dataT[rowNum,preri]*rank[getnewID(dataT[rowNum,from]),3-oddIt]]
-#                   }
-#                   else{
-#                         oldIDi<- dataT[rowNum,from]
-#                         ranki<-rank[[getnewID(oldIDi),3-oddIt]]
-#                         outDi<-dataT[rowNum,preri]
-#                         set(dataT,rowNum,4L,outDi*ranki)
-#                   }
-#             }
-#             dataT<-aggregate(. ~ to, data=select(dataT,to,preri), FUN = sum)
+            setnames(agregPreri,"to","oldID")
+            setkey(agregPreri,oldID)
+            setkey(IDmap,oldID)
+            rank<-agregPreri[IDmap,nomatch=NA]
+            set(rank,which(is.na(rank[["agRi"]])),"agRi",0)
+            S<-(1 - sum(agregPreri[,agRi]))/nNodes
+            setnames(rank,"agRi","Rank")
+            rank[,Rank:=Rank +S]
+            drank<-data.table(rank[,Rank],prevrank[,Rank])
+            drank[,dist:=abs(V1-V2)]
+            dist<-sum(drank[,dist])
+            iteration<-iteration+1
+            print(iteration)
+            print(dist)
             
-            #dataT<-select(dataT,to,preri)[, lapply(.SD, sum), by = preri]
             
-#             #Iteration calcul de r'
-#             for (j in 1:nNodes){
-#                   oldj<-getoldID(j)
-#                   rankj<-0
-#                   toJ<-dataT[to==oldj,]
-#                   if(nrow(toJ)!=0){
-#                         for (k in seq(nrow(toJ))){
-#                               ri<-rank[getnewID(toJ[k,from]),3-oddIt]
-#                               di<-toJ[k,outD]
-#                               rankj<- rankj + 0.8*ri/di
-#                         }
-#                   }
-#                   else rankj<-0
-#                   rank[j,oddIt]<-rankj
-#             }
-            
-            #soustraction de S a r' 
-            S<-0
-            for (i in seq(nNodes)){
-                  S<-S+ rank[[i,oddIt]]
-            }
-            for (i in seq(nNodes)){
-                  rank[i,oddIt]<-rank[[i,oddIt]] +(1-S)/nNodes
-            }
-            oddIt<-3-oddIt
+#             dataT[ , `:=`(preri = R1/outD)]
+#             agregPreri<-group_by(dataT,to) %>%summarise( agRi = sum(preri))
+#             dataT<- agregPreri[dataT,nomatch=NA]
+#             dataT[,R2:=NULL]
+#             setnames(dataT,"R1","R2")
+#             setnames(dataT,"agRi","R1")
+# 
+#             #soustraction de S a r' 
+#             S<-(1 - sum(agregPreri[,agRi]))/nNodes
+#             dataT[ , `:=`(R1 = R1 +S)]
       }
-      cbind(rank[[,3-oddIt]],IDmap[,oldID])
+
+      rank
 
       
 }
